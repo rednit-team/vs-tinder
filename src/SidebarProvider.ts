ad86fd2cfd2f920bce1f86acab40e03838bf96d5
Kaktushose <faulie50@gmail.com>
2021-05-19 17:45:50 +0200

    setup svelte and prettier

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -1,23 +1,37 @@
 import * as vscode from "vscode";
 import { getNonce } from "./getNonce";
 
-export class SidebarProvider {
+export class SidebarProvider implements vscode.WebviewViewProvider {
+  _view?: vscode.WebviewView;
+  _doc?: vscode.TextDocument;
 
-  constructor(private readonly _extensionUri: vscode.Uri) { }
+  constructor(private readonly _extensionUri: vscode.Uri) {}
 
   public resolveWebviewView(webviewView: vscode.WebviewView) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
     webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+  }
+
+  public revive(panel: vscode.WebviewView) {
+    this._view = panel;
   }
 
   private _getHtmlForWebview(webview: vscode.Webview) {
     const styleResetUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+      vscode.Uri.joinPath(this._extensionUri, "out/compiled", "reset.css")
     );
     const styleVSCodeUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+      vscode.Uri.joinPath(this._extensionUri, "out/compiled", "vscode.css")
     );
 
-    // Use a nonce to only allow a specific script to be run.
     const nonce = getNonce();
 
     return `<!DOCTYPE html>
@@ -28,8 +42,7 @@ export class SidebarProvider {
 					Use a content security policy to only allow loading images from https or from our extension directory,
 					and only allow scripts that have a specific nonce.
         -->
-        <meta http-equiv="Content-Security-Policy" content=" img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource
-      }; script-src 'nonce-${nonce}';">
+        <meta http-equiv="Content-Security-Policy" content=" img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="${styleResetUri}" rel="stylesheet">
 				<link href="${styleVSCodeUri}" rel="stylesheet">
--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
 import { SidebarProvider } from "./SidebarProvider";
 
 export function activate(context: vscode.ExtensionContext) {
@@ -9,6 +10,25 @@ export function activate(context: vscode.ExtensionContext) {
       sidebarProvider
     )
   );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vstinder.helloWorld", () => {
+      HelloWorldPanel.createOrShow(context.extensionUri);
+      vscode.window.showInformationMessage("Sup");
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vstinder.refresh", () => {
+      HelloWorldPanel.kill();
+      HelloWorldPanel.createOrShow(context.extensionUri);
+      // setTimeout(() => {
+      //   vscode.commands.executeCommand(
+      //     "workbench.action.webview.openDeveloperTools"
+      //   );
+      // }, 500);
+    })
+  );
 }
 
-export function deactivate() { }
+export function deactivate() {}
